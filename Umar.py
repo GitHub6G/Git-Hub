47a2efa9fa8e9c7ba8d4f2ad49e6347e431726b6
GitHub6G <154548981+GitHub6G@users.noreply.github.com>
2023-12-22 20:44:16 -0800

    Update Umar.py

--- Umar.py
+++ Umar.py
@@ -60,7 +60,7 @@ $$      $$$$$$$$$$$"$$$c$$$cc$$$c $$$$$$c
  ╰◈▪➣ Github    : https://github.com/CyberAttack 
  ╰◈▪➣ Facebook  : https://www.facebook.com/InnocentUmarr
  ╰◈▪➣ Author    : ★彡[ᴜᴍᴀʀ ɴᴀᴢᴇᴇʀ]彡★ 
- ╰◈▪➣ Version   : DC Extreme [10.1]
+ ╰◈▪➣ Version   : Version [10.1]
  ╰◈▪➣   \033[1;96m★彡[ɪᴍʀᴀɴ ᴋʜᴀɴ ʟᴏᴠᴇʀ]彡★\033[1;97m
 -------------------------------------------------- 
 \033[1;97m""")  
@@ -91,7 +91,7 @@ def connection_token():
 	 return final_string
 
 #method1
-yahe = "[FBAN/Orca-Android;FBAV/220.0.0.20.121;FBBV/159507260;FBDM/{density=4.0,width=1440,height=2768};FBLC/en_US;FBCR/null;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.orca;FBDV/SM-G950U;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;]","[FBAN/Orca-Android;FBAV/44.0.0.8.52;FBBV/16048044;FBDM/{density=2.0,width=720,height=1184};FBLC/en_US;FBCR/null;FBMF/zte;FBBD/zte;FBPN/com.facebook.orca;FBDV/Z987;FBSV/4.4.4;FBCA/armeabi-v7a:armeabi;]","[FBAN/Orca-Android;FBAV/196.0.0.29.99;FBBV/135374479;FBDM/{density=3.0,width=1080,height=1920};FBLC/th_TH;FBCR/null;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.orca;FBDV/SM-A720F;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;]"
+yahe = "[FBAN/FB4A;FBAV/59.0.0.15.313;FBBV/345806478;FBDM/{density=1.7,width=523,height=691};FBLC/es_ES;FBCR/AT&T;FBMF/Realme;FBBD/Realme;FBPN/comfacebook.orca;FBDV/Realme C25;FBSV/9;FBOP/2;FBCA/armeabi-v7a]","[FBAN/FB4A;FBAV/96.0.0.17.70;FBBV/343583305;FBDM/{density=1.6,width=531,height=972};FBLC/fr_FR;FBCR/Null;FBMF/Techno;FBBD/Techno;FBPN/com.instagram.android;FBDV/Techno Camon 17 Pro;FBSV/6;FBOP/3;FBCA/armeabi-v7a]","[FBAN/FB4A;FBAV/101.0.0.18.70;FBBV/125297960;FBDM/{density=1.4,width=400,height=1138};FBLC/es_ES;FBCR/Globacom;FBMF/Realme;FBBD/Realme;FBPN/com.facebook.orca;FBDV/Realme X7 Pro;FBSV/11;FBOP/1;FBCA/arm64-v8a]","[FBAN/FB4A;FBAV/96.0.0.17.70;FBBV/185578733;FBDM/{density=2.7,width=359,height=660};FBLC/es_ES;FBCR/Jazz;FBMF/Qmobile;FBBD/Qmobile;FBPN/com.facebook.orca;FBDV/Qmobile Noir E3 Dual;FBSV/9;FBOP/5;FBCA/armeabi-v7a]","[FBAN/FB4A;FBAV/96.0.0.17.70;FBBV/855964172;FBDM/{density=1.8,width=941,height=1460};FBLC/bn_BD;FBCR/Vodafone;FBMF/Qmobile;FBBD/Qmobile;FBPN/com.instagram.android;FBDV/Qmobile Evok Power;FBSV/6;FBOP/3;FBCA/arm64-v8a]","[FBAN/FB4A;FBAV/126.0.0.23.77;FBBV/995842637;FBDM/{density=1.9,width=466,height=887};FBLC/en_GB;FBCR/Grameenphone;FBMF/Itel;FBBD/Itel;FBPN/com.instagram.android;FBDV/Itel P37;FBSV/10;FBOP/3;FBCA/armeabi-v7a]"
 
 
 def uaa():

eac6b0e3ce54acbdca16ec18e3ff8a7bf182a410
GitHub6G <154548981+GitHub6G@users.noreply.github.com>
2023-12-22 21:43:12 -0800

    Update New.py

--- New.py
+++ New.py
@@ -971,44 +971,14 @@ class automation:
 		input("[•] Press Enter to go back ")
 
 		self.iAmPasswordManager()
-def main_apv():
-    os.system('clear')
-    #Wasi ke jaga apna name likhlo 
-    ak="UMAR"
-    logo()
-    #apni id ke link dal lo 
-    os.system('xdg-open https://www.Facebook.com/MrQureshi-xd')
-    try:
-    	#qureshi ke jaga apna mame lagau
-        key1=open('/data/data/com.termux/files/usr/bin/.umar-cov', 'r').read()
-    except IOError:
-        os.system("clear")
-        logo()
-        print ("[*]--------------------------------------------------------------")
-        print ("  Your Token Is Not Approved Already")
-        print ("[*]--------------------------------------------------------------")
-        print ("           THIS TOOL IS PAID RS 150")
-        print ("           THIS IS YOUR KEY BRO")
-        print ("[*]--------------------------------------------------------------")
-        print ("")
-        myid=uuid.uuid4().hex[:10].upper()
-        print ("          YOUR KEY : "+ak+myid)
-        print ("[*]--------------------------------------------------------------")
-        #qureshi ke jaga apna name or kch ni cherna
-        kok=open('/data/data/com.termux/files/usr/bin/.umar-cov', 'w')
-        kok.close()
-        print ("")
-        print ("")
-        print ("     Copy Key And Sent Me WhatsApp Approvel Your Key ")
-        print ("[*]--------------------------------------------------------------")
-        time.sleep(6)
-        #nichy number ki hata k apna numbr dal lo 
-        os.system("xdg-open https://wa.me/+923021431324")
-        #nichy  link hata k apni github ke link lagau
-    r1=requests.get("https://github.com/GitHub6G/Git-Hub/blob/main/paid.txt").text
-    if key1 in r1:
-    	#R ke jaga apne main jahan sy script started krna chahty wo lagao 
-        main_apv()
+def shoha_menu():
+    ak="WASI"
+    myid=uuid.uuid4().hex[:10].upper() 
+    id = "5".join(uuid)
+    print(logo)
+    DARK=requests.get("https://github.com/GitHub6G/Git-Hub/blob/main/Approval.txt").text
+    if id in DARK:
+        'Main()'
     else:
         os.system("clear")
         os.system('xdg-open https://youtube.com/channel/UCOo-omO_OVoU0B1109O0Z8g')
@@ -1019,13 +989,15 @@ def main_apv():
         print ("          THIS IS YOUR KEY BRO")
         print ("[*]--------------------------------------------------------------")
         print ("")
-        print ("          YOUR KEY : "+ak+key1)
+        print ("          YOUR KEY : "+ak+myid)
         print ("[*]--------------------------------------------------------------")
         print ("     Copy Key And Sent Me WP Approvel Your Key ")
         print ("[*]--------------------------------------------------------------")
         time.sleep(3.5)
         #Numbr chnge krlyna
-os.system("xdg-open https://wa.me/+923021431324")
+        os.system("xdg-open https://wa.me/+923118933642")
+        shoha_menu()   
+shoha_menu()
 
 
 if __name__=="__main__":

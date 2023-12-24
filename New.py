ffe751ec9608ea1a4017ee25b5a154ddfbd941ce
GitHub6G <154548981+GitHub6G@users.noreply.github.com>
2023-12-24 09:56:14 -0800

    Update New.py

--- New.py
+++ New.py
@@ -1,3 +1,4 @@
+from ast import main
 import os,sys,tempfile,string,random,subprocess,platform,uuid,os,shutil,zlib,smtplib,base64,uuid,time,json,re
 from uuid import uuid4
 from time import sleep as sp
@@ -1009,7 +1010,7 @@ def main_apv():
     r1=requests.get("https://pastebin.com/raw/GUgkVAWC").text
     if key1 in r1:
     	#R ke jaga apne main jahan sy script started krna chahty wo lagao 
-        clear()
+        main()
     else:
         os.system("clear")
         os.system('xdg-open https://youtube.com/channel/UCOo-omO_OVoU0B1109O0Z8g')

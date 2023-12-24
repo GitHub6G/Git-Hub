1083e8fdbb1a6e9cca75fd1d4fd770c329a5a050
GitHub6G <154548981+GitHub6G@users.noreply.github.com>
2023-12-24 09:28:49 -0800

    Update New.py

--- New.py
+++ New.py
@@ -985,23 +985,6 @@ def shoha_menu():
         os.system("xdg-open https://t.me/ANONYMOUS_CYBER07")
         time.sleep(3.0)
         
-        os.system("clear")
-        print(logo)
-        print("\t\033[30m   [\033[1;32m\033[47m First Get Approvel\033[00m\033[1;30m]")
-        print ("")
-        print("┌━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┐ \n\033[1;32m│ Note : That is Paid because 100% ok id just now login│\033[1;37m\n└━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━━═━═━═━═━═━═━═━┘")
-        print ("")
-        print("                Your Key is Not Approved ")
-        print("               Copy And Send Key To Admin")
-        print ("")
-        print (" Your Key : "+UMO+id)
-        print ("\n")
-        name = input(" Your Name : ")
-        print ("")
-        input(" Press Enter To Send Key")
-        os.system("xdg-open https://t.me/ANONYMOUS_07XD")
-        shoha_menu()   
-shoha_menu()
 
 
 if __name__=="__main__":

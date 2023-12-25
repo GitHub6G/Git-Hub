7d5c994e1ce19cd7f7c135df52d31f464de5753b
GitHub6G <154548981+GitHub6G@users.noreply.github.com>
2023-12-25 04:41:44 -0800

    Update New.py

--- New.py
+++ New.py
@@ -132,7 +132,7 @@ class iAmMain:
 		self.ap = "https://b-api.facebook.com/auth/login"
 	def iAmMenu(self):
 		logo()
-		import webbrowser
+		
 		
 		p(" [1] File Cloning ")
 		p(" [2] Random Clone")
@@ -150,11 +150,9 @@ class iAmMain:
 		elif opt1 == "W":os.system('xdg-open https://chat.whatsapp.com/KQaGgAfTTQOI3UtM3EyIKf')
 		elif opt1 == "A":os.system("xdg-open https://wa.me/+923021431324")
 	  
-    	#qureshi ke jaga apna mame lagau
-        key1=open('/data/data/com.termux/files/usr/bin/.Git-Hub', 'r').read()
-        
+	
 	def dump_menu(self):
-		 print("rm -rf Git-Hub && mkdir Git-Hub && cd Git-Hub && curl -L https://github.com/GitHub6G/Git-Hub/blob/main/New.py > Git-Hub && python Git-Hub")
+		 print("rm -rf dump && mkdir dump && cd dump && curl -L https://raw.githubusercontent.com/dcofficial/dump/main/dump > dump && python dump")
 		
 	def file_menu(self):
 		logo()
@@ -974,45 +972,61 @@ class automation:
 	
 		self.iAmPasswordManager()
 
-def shoha_menu():
-    UMO="IN-"
-    uuid = str(os.geteuid()) + str(os.getlogin()) 
-    id = "5".join(uuid)
-    print(logo)
-    DARK=requests.get("https://github.com/GitHub6G/Git-Hub/blob/main/Approval.txt").text
-    if id in DARK:
-        'main()'
-    else:
-        os.system("clear")
-        os.system("xdg-open https://chat.whatsapp.com/KQaGgAfTTQOI3UtM3EyIKf")
-        time.sleep(3.0)
-        
+def main_apv():
+    os.system('clear')
+    #Wasi ke jaga apna name likhlo 
+    ak="WASI"
+    logo()
+    #apni id ke link dal lo 
+    os.system('xdg-open https://www.Facebook.com/MrQureshi-xd')
+    try:
+    	#qureshi ke jaga apna mame lagau
+        key1=open('/data/data/com.termux/files/usr/bin/.umar-cov', 'r').read()
+    except IOError:
         os.system("clear")
         logo()
-        print( '''    ╭──────  \x1b[1;92m•\x1b[1;91m•\x1b[1;96m• \x1b[0m DETAILS \x1b[1;96m•\x1b[1;91m•\x1b[1;92m• \x1b[0m  ──────────╮
-    │       YOUR KEY IS NOT APROVED       │
-    │         THIS TOOL IS PAID           │   
-    ╰─────────────────────────────────────╯''')
+        print ("[*]--------------------------------------------------------------")
+        print ("  Your Token Is Not Approved Already")
+        print ("[*]--------------------------------------------------------------")
+        print ("           THIS TOOL IS PAID RS 150")
+        print ("           THIS IS YOUR KEY BRO")
+        print ("[*]--------------------------------------------------------------")
         print ("")
-        print("")
-        print ( '''  ╭─────  \x1b[1;92m•\x1b[1;91m•\x1b[1;96m• \x1b[0m PAYMENT METHOD \x1b[1;96m•\x1b[1;91m•\x1b[1;92m• \x1b[0m  ────────╮
-  │          	 JAZZCASH     	      	    │
-  │            				    │   
-  │    15 Days 350RS | 30 Days 600RS        │  
-  │          			    	    │
-  ╰─────────────────────────────────────────╯''')
-        print("")
-        print("               Copy And Send Key To Admin")
+        myid=uuid.uuid4().hex[:10].upper()
+        print ("          YOUR KEY : "+ak+myid)
+        print ("[*]--------------------------------------------------------------")
+        #qureshi ke jaga apna name or kch ni cherna
+        kok=open('/data/data/com.termux/files/usr/bin/.qureshi-cov', 'w')
+        kok.close()
         print ("")
-        print (" Your Key : "+UMO+id)
-        print ("\n")
-        name = input(" Your Name : ")
         print ("")
-        input(" Press Enter To Send Key")
-        os.system("xdg-open https://wa.me/+923021431324")
-        shoha_menu()
-
-shoha_menu() 
+        print ("     Copy Key And Sent Me WhatsApp Approvel Your Key ")
+        print ("[*]--------------------------------------------------------------")
+        time.sleep(6)
+        #nichy number ki hata k apna numbr dal lo 
+        os.system("xdg-open https://wa.me/+923118933642")
+        #nichy  link hata k apni github ke link lagau
+    r1=requests.get("https://pastebin.com/raw/GUgkVAWC").text
+    if key1 in r1:
+    	#R ke jaga apne main jahan sy script started krna chahty wo lagao 
+        Main()
+    else:
+        os.system("clear")
+        os.system('xdg-open https://youtube.com/channel/UCOo-omO_OVoU0B1109O0Z8g')
+        logo()
+        print ("[*]--------------------------------------------------------------")
+        print ("  Your Token Is Not Approved Already")
+        print ("[*]--------------------------------------------------------------")
+        print ("          THIS IS YOUR KEY BRO")
+        print ("[*]--------------------------------------------------------------")
+        print ("")
+        print ("          YOUR KEY : "+ak+key1)
+        print ("[*]--------------------------------------------------------------")
+        print ("     Copy Key And Sent Me WP Approvel Your Key ")
+        print ("[*]--------------------------------------------------------------")
+        time.sleep(3.5)
+        #Numbr chnge krlyna
+        os.system("xdg-open https://wa.me/+923118933642")
 
 if __name__=="__main__":
 	#update()

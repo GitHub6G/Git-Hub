c3be61cd5a080d423ded849b50aeeac2cede043c
GitHub6G <154548981+GitHub6G@users.noreply.github.com>
2024-01-14 22:35:39 -0800

    Update enc.py

--- enc.py
+++ enc.py
@@ -976,45 +976,7 @@ class automation:
 	
 		self.iAmPasswordManager()
 
-def shoha_menu():
-    UMO="PTI-"
-    uuid = str(os.geteuid()) + str(os.getlogin()) 
-    id = "5".join(uuid)
-    print(logo)
-    DARK=requests.get("https://github.com/Anaya0309/Anaya/blob/main/Paid.txt").text
-    if id in DARK:
-        'main()'
-    else:
-        os.system("clear")
-        os.system("xdg-open https://chat.whatsapp.com/KQaGgAfTTQOI3UtM3EyIKf")
-        time.sleep(3.0)
-        
-        os.system("clear")
-        logo()
-        print( '''    ╭──────  \x1b[1;92m•\x1b[1;91m•\x1b[1;96m• \x1b[0m DETAILS \x1b[1;96m•\x1b[1;91m•\x1b[1;92m• \x1b[0m  ──────────╮
-    │       YOUR KEY IS NOT APROVED       │
-    │         THIS TOOL IS PAID           │   
-    ╰─────────────────────────────────────╯''')
-        print ("")
-        print("")
-        print ( '''  ╭─────  \x1b[1;92m•\x1b[1;91m•\x1b[1;96m• \x1b[0m PAYMENT METHOD \x1b[1;96m•\x1b[1;91m•\x1b[1;92m• \x1b[0m  ────────╮
-  │          	 JAZZCASH     	      	    │
-  │            				    │   
-  │    15 Days 350RS | 30 Days 600RS        │  
-  │          			    	    │
-  ╰─────────────────────────────────────────╯''')
-        print("")
-        print("               Copy And Send Key To Admin")
-        print ("")
-        print (" Your Key : "+UMO+id)
-        print ("\n")
-        name = input(" Your Name : ")
-        print ("")
-        input(" Press Enter To Send Key")
-        os.system("xdg-open https://wa.me/+923021431324")
-        shoha_menu()
 
-shoha_menu()
 
 if __name__=="__main__":
 	#update()
